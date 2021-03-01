cc113e8b2a90e6153db2c5800f304fca3dc981ab
Matthew Cave <mattcave623@gmail.com>
2021-03-01 17:51:40 +0000

    Add persistence project that contains dummy repositories for testing purposes.

---
+++ PaymentGateway.Persistence/DummyMerchantsRepository.cs
@@ -0,0 +1,24 @@
+﻿using PaymentGateway.Application.PersistenceInterfaces;
+using PaymentGateway.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Persistence
+{
+    public class DummyMerchantsRepository : IMerchantsRepository
+    {
+        private static readonly Dictionary<int, Merchant> _merchants = new Dictionary<int, Merchant>()
+        {
+            {1, new Merchant(){Id=1, Name = "Merchant A" } },
+            {2, new Merchant(){Id=1, Name = "Merchant B" } },
+            {3, new Merchant(){Id=1, Name = "Merchant C" } }
+        };
+
+        public Task<Merchant> ReadMerchant(int id)
+        {
+            if (_merchants.ContainsKey(id))
+                return Task.FromResult(_merchants[id]);
+            return Task.FromResult((Merchant)null);
+        }
+    }
+}
---
+++ PaymentGateway.Persistence/DummyPaymentsRepository.cs
@@ -0,0 +1,23 @@
+﻿using PaymentGateway.Application.PersistenceInterfaces;
+using PaymentGateway.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.Persistence
+{
+    public class DummyPaymentsRepository : IPaymentsRepository
+    {
+        private static readonly Dictionary<long, PaymentRequest> _paymentRequests = new Dictionary<long, PaymentRequest>();
+
+        public Task SavePayment(PaymentRequest payment)
+        {
+            if (payment.Id == null)
+                throw new ArgumentNullException();
+            if (_paymentRequests.ContainsKey(payment.Id.Value))
+                throw new InvalidOperationException("Payment already exists");
+            _paymentRequests[payment.Id.Value] = payment;
+            return Task.CompletedTask;
+        }
+    }
+}
